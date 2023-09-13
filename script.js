07b829622009d5607b6f7f715f1b22125584c0fd
ShivanshSinghnnovative <shivansh.singh@innovative.in>
2023-09-13 11:22:17 +0530

    m

--- script.js
+++ script.js
@@ -1,60 +1,87 @@
-let x= document.getElementById('overlay');
-let y= document.getElementById('center');
-let currentIndex=0;
+var imagePaths = [
+  "./1.jpg",
+  "./2.jpg",
+  "./3.jpg",
+  "./4.jpg",
+  "./5.jpg",
+  "./6.jpg",
+  "./7.jpg",
+  "./8.jpg",
+  "./9.jpg",
+  "./10.jpg",
+  "./11.jpg",
+  "./12.jpg",
+  "./13.jpg",
+  "./14.jpg",
+  "./15.jpg",
+  "./16.jpg",
+];
+function addImages() {
+  let container = document.querySelector(".container");
 
-function openImage(imageSrc) {  
-   currentIndex=imageSrc;
-  document.getElementById('overlayImage').src = `${imageSrc}.jpg`;
+  for (let i = 0; i < imagePaths.length; i++) {
+    let imgElement = document.createElement("img");
+    imgElement.classList.add("x");
+    imgElement.src = imagePaths[i];
+    imgElement.onclick = (function (index) {
+      return function () {
+        openImage(index);
+      };
+    })(i + 1);
+    container.appendChild(imgElement);
+  }
+}
+
+addImages();
+
+let x = document.getElementById("overlay");
+let y = document.getElementById("center");
+let currentIndex = 0;
+
+function openImage(imageSrc) {
+  currentIndex = imageSrc;
+  document.getElementById("overlayImage").src = `${imageSrc}.jpg`;
   x.style.display = "block";
   y.style.display = "block";
   buttonWork();
 }
 
-function previousButton(){
-    if(currentIndex>1){
-        currentIndex--;
-        displayImage(currentIndex);
-    }
+function previousButton() {
+  if (currentIndex > 1) {
+    currentIndex--;
+    displayImage(currentIndex);
+  }
 }
 
-
-function nextButton(){
-    if(currentIndex<16){
-        currentIndex++;
-        displayImage(currentIndex);
-       
-    }
-    // if(currentIndex==="16"){
-    //     document.getElementById('next').style.display="none";
-    // }
-    // else{
-    //     document.getElementById('prev').style.display="block";
-    // }
+function nextButton() {
+  if (currentIndex < 16) {
+    currentIndex++;
+    displayImage(currentIndex);
+  }
 }
-function displayImage(currentIndex) {  
-    currentIndex ;
-  document.getElementById('overlayImage').src = `${currentIndex}.jpg`;
+function displayImage(currentIndex) {
+  currentIndex;
+  document.getElementById("overlayImage").src = `${currentIndex}.jpg`;
   x.style.display = "block";
   y.style.display = "block";
-  buttonWork() ;
+  buttonWork();
 }
-function hideImage(){
-    x.style.display = "none";
+function hideImage() {
+  x.style.display = "none";
   y.style.display = "none";
 }
 
-function buttonWork(){
-if(currentIndex=="1"){
-    console.log("hd")
-    document.getElementById('prev').style.visibility="hidden";
-}else{
-    console.log("hd")
-    document.getElementById('prev').style.visibility="visible";
-}
-if(currentIndex=="16"){
-    document.getElementById("next").style.visibility="hidden";
-}
-else{
-    document.getElementById("next").style.visibility="visible";
-}
+function buttonWork() {
+  if (currentIndex == "1") {
+    console.log("hd");
+    document.getElementById("prev").style.visibility = "hidden";
+  } else {
+    console.log("hd");
+    document.getElementById("prev").style.visibility = "visible";
+  }
+  if (currentIndex == "16") {
+    document.getElementById("next").style.visibility = "hidden";
+  } else {
+    document.getElementById("next").style.visibility = "visible";
+  }
 }
